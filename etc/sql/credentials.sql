c2c1870f9587e3f744e56c3dc35e2e4e938950c7
jmosshammer <jmosshammer@ws-jmosshammer.(none)>
2010-07-02 17:11:03 +0200

    * Added db credentials

---
+++ etc/sql/credentials.sql
@@ -0,0 +1,2 @@
+INSERT INTO nsm_target (target_name,target_description,target_type) VALUES ('lconf.user','Allow access to the lconf module','credential');
+INSERT INTO nsm_target (target_name,target_description,target_type) VALUES ('lconf.admin','Allow administration of lconf module','credential');
