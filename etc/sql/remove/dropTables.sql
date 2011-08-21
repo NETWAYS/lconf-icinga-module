5c12e27165994570de9e93d1cf8b8919b4417e77
Jannis Moßhanner <jannis.mosshammer@netways.de>
2011-08-21 20:18:58 +0200

    Updated installer for icinga v.1.5

---
+++ etc/sql/remove/dropTables.sql
@@ -0,0 +1,6 @@
+-- Removes all lconf tables in the correct order
+
+DROP TABLE lconf_defaultconnection;
+DROP TABLE lconf_filter;
+DROP TABLE lconf_principal;
+DROP TABLE lconf_connection;
--- etc/sql/update_v1.2_to_v.1.3.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE lconf_connection ADD owner INTEGER;
