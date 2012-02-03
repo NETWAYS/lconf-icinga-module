eeea22f4775389e0f0e03f4372cf964fd938ec4b
Jannis Mosshammer <jannis.mosshammer@netways.de>
2012-02-03 12:17:46 +0100

    * Added db column size update scripts (fixes #829)

---
+++ etc/sql/update/update_v1.2_to_v1.3.sql
@@ -0,0 +1,2 @@
+ALTER TABLE lconf_connection MODIFY COLUMN connection_binddn VARCHAR(2048); 
+ALTER TABLE lconf_connection MODIFY COLUMN connection_basedn VARCHAR(2048);
---
+++ etc/sql/update/update_v1.2_to_v1.3_psql.sql
@@ -0,0 +1,2 @@
+ALTER TABLE lconf_connection ALTER COLUMN connection_binddn TYPE VARCHAR(2048); 
+ALTER TABLE lconf_connection ALTER COLUMN connection_basedn TYPE VARCHAR(2048);
