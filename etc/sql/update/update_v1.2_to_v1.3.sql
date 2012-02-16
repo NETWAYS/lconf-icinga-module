ede1df88b759c6228aeaa4195007f84bfb1db8a5
Jannis Mosshammer <jannis.mosshammer@netways.de>
2012-02-16 13:46:26 +0100

    * Cleanup

--- etc/sql/update/update_v1.2_to_v1.3.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE lconf_connection MODIFY COLUMN connection_binddn VARCHAR(2048); 
-ALTER TABLE lconf_connection MODIFY COLUMN connection_basedn VARCHAR(2048);
--- etc/sql/update/update_v1.2_to_v1.3_psql.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE lconf_connection ALTER COLUMN connection_binddn TYPE VARCHAR(2048); 
-ALTER TABLE lconf_connection ALTER COLUMN connection_basedn TYPE VARCHAR(2048);
